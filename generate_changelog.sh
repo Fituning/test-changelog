ec43a6686907acf3fc1a87b777c165c1d2dfd423
Sroÿce <63264339+Fituning@users.noreply.github.com>
2024-10-22 09:52:28 +0200

    Fixed(generate_changelog.sh)
    fixed display of commits

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# URL de ton dépôt GitHub (remplace par ton URL)
+REPO_URL="https://github.com/Fituning/test-changelog"
+
 # Fichier changelog
 CHANGELOG_FILE="CHANGELOG.md"
 
@@ -7,8 +10,12 @@ CHANGELOG_FILE="CHANGELOG.md"
 echo "# Changelog" > $CHANGELOG_FILE
 echo "" >> $CHANGELOG_FILE
 
-# Obtenir les commits et vérifier qu'il y en a
-git log --pretty=format:"%h;%cd;%s" --date=short --reverse | while IFS=";" read commit_hash commit_date commit_message; do
+# Ajouter les colonnes au changelog
+echo "| Date et Heure      | Commit (ID long)    | **Tag**      | *Scope*       | Description         |" >> $CHANGELOG_FILE
+echo "|-------------------|--------------------|--------------|---------------|---------------------|" >> $CHANGELOG_FILE
+
+# Obtenir les commits du plus récent au plus ancien
+git log --pretty=format:"%H;%cd;%s" --date=iso | while IFS=";" read commit_hash commit_date commit_message; do
     # Ignorer les commits qui commencent par '#'
     if [[ $commit_message == \#* ]]; then
         continue
@@ -19,35 +26,10 @@ git log --pretty=format:"%h;%cd;%s" --date=short --reverse | while IFS=";" read 
     file_component=$(echo $commit_message | cut -d'(' -f2 | cut -d')' -f1)
     description=$(echo $commit_message | cut -d')' -f2-)
 
-    # Classer les commits selon les tags et ajouter au changelog
-    case $tag in
-        Added)
-            echo "## Added" >> $CHANGELOG_FILE
-            ;;
-        Changed)
-            echo "## Changed" >> $CHANGELOG_FILE
-            ;;
-        Deprecated)
-            echo "## Deprecated" >> $CHANGELOG_FILE
-            ;;
-        Removed)
-            echo "## Removed" >> $CHANGELOG_FILE
-            ;;
-        Fixed)
-            echo "## Fixed" >> $CHANGELOG_FILE
-            ;;
-        Security)
-            echo "## Security" >> $CHANGELOG_FILE
-            ;;
-    esac
-
-    # Ajouter le commit avec ses informations
-    echo "| $commit_date | $commit_hash | $file_component | $description |" >> $CHANGELOG_FILE
+    # Formater la ligne avec les colonnes requises
+    commit_link="[$commit_hash]($REPO_URL/commit/$commit_hash)"
+    echo "| $commit_date | $commit_link | **$tag** | *$file_component* | $description |" >> $CHANGELOG_FILE
 done
-
-# Ajouter un en-tête pour les colonnes
-sed -i '3i| Date       | Commit    | Fichier/Composant | Description |' $CHANGELOG_FILE
-sed -i '4i|------------|-----------|-------------------|-------------|' $CHANGELOG_FILE
 
 
 # Générer un fichier JSON
