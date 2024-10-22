f4e4f0638d77c464c1cacf333313febcb0d99ea4
Sroÿce <63264339+Fituning@users.noreply.github.com>
2024-10-22 14:37:42 +0200

    Update generate_changelog.sh

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -75,13 +75,15 @@ echo "$commits" | while IFS=";" read commit_hash commit_date commit_message; do
     # Incrémenter le compteur pour savoir si nous sommes au dernier commit
     counter=$((counter + 1))
 
-    # Ajouter chaque commit au JSON avec la description modifiée
-    echo "{\"commit\": \"$commit_hash\", \"date\": \"$commit_date\", \"tag\": \"$tag\", \"scope\": \"$file_component\", \"description\": \"$description\"}" >> $JSON_FILE
-
     # Ajouter une virgule entre les commits, sauf pour le dernier
     if [ $counter -lt $commit_count ]; then
-        echo "," >> $JSON_FILE
+        virgule =  ","
+    else 
+        virgule =  ""
     fi
+    
+    # Ajouter chaque commit au JSON avec la description modifiée
+    echo "{\"commit\": \"$commit_hash\", \"date\": \"$commit_date\", \"tag\": \"$tag\", \"scope\": \"$file_component\", \"description\": \"$description\"}\"$virgule\"" >> $JSON_FILE
 done
 
 # Fermer le tableau JSON proprement
