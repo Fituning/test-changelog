1c9ae553dc329cb99ff8b26566824bafd5303163
Sroÿce <63264339+Fituning@users.noreply.github.com>
2024-10-22 15:42:09 +0200

    Update generate_changelog.sh
    test of description

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -38,7 +38,7 @@ echo "$commits" | while IFS=";" read commit_hash commit_date commit_message; do
         continue
     fi
 
-    # Accumuler les lignes suivantes pour le corps du commit
+    # Accumuler uniquement le corps du commit (description), pas le message de titre
     commit_body=""
     while read body_line; do
         if [ -z "$body_line" ]; then
@@ -47,34 +47,14 @@ echo "$commits" | while IFS=";" read commit_hash commit_date commit_message; do
         commit_body="$commit_body $body_line"
     done <<< "$(git show -s --format=%b $commit_hash)"
 
-    commit_message=$(echo "$commit_message" | sed 's/"/\\"/g' | sed "s/'/\\'/g")
-    commit_body=$(echo "$commit_body" | sed 's/"/\\"/g' | sed "s/'/\\'/g")
-    
-    full_description="$commit_message"
-    if [[ ! -z "$commit_body" ]]; then
-        full_description="$full_description $commit_body"
-    fi
-    full_description=$(echo "$full_description" | tr '\n' ' ' | sed 's/[[:space:]]\+/ /g')
-
-    if [[ $commit_message =~ ^([A-Za-z]+)\ ([A-Za-z0-9._-]+)\ ?(.*) ]]; then
-        tag="${BASH_REMATCH[1]}"
-        file_component="${BASH_REMATCH[2]}"
-        description="${BASH_REMATCH[3]}"
-    elif [[ $commit_message =~ ^Merge.* ]]; then
-        tag="Merge"
-        file_component=""
-        description=$commit_message
-    else
-        tag=""
-        file_component=""
-        description=$commit_message
-    fi
+    # Nettoyage des retours à la ligne dans le commit body
+    commit_body=$(echo "$commit_body" | tr '\n' ' ' | sed 's/[[:space:]]\+/ /g')
 
     # Incrémenter le compteur pour savoir si nous sommes au dernier commit
     counter=$((counter + 1))
-
-    # Ajouter chaque commit au JSON avec la description modifiée
-    echo "{\"commit\": \"$commit_hash\", \"date\": \"$commit_date\", \"tag\": \"$tag\", \"scope\": \"$file_component\", \"description\": \"$description\"}," >> $JSON_FILE
+    
+    # Ajouter chaque commit au JSON avec uniquement la description (corps du commit)
+    echo "{\"commit\": \"$commit_hash\", \"date\": \"$commit_date\", \"tag\": \"$commit_message\", \"scope\": \"\", \"description\": \"$commit_body\"}," >> $JSON_FILE
 done
 
 # Ajouter la fermeture du tableau JSON
