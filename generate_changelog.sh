81bd66b2dbc5f3b95ea218f26ebb4e3459d7a7e5
Sroÿce <63264339+Fituning@users.noreply.github.com>
2024-10-22 09:40:55 +0200

    Added(generate_changelog.sh)
    Added new display for logs

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -7,30 +7,57 @@ CHANGELOG_FILE="CHANGELOG.md"
 echo "# Changelog" > $CHANGELOG_FILE
 echo "" >> $CHANGELOG_FILE
 
-# Obtenir les commits
-git log --pretty=format:"%h %s" --reverse | while read line; do
-    commit_hash=$(echo $line | awk '{print $1}')
-    commit_message=$(echo $line | cut -d' ' -f2-)
-
-    # Classer les commits selon les tags
-    if [[ $commit_message == Added* ]]; then
-        echo "## Added" >> $CHANGELOG_FILE
-        echo "- $commit_message ($commit_hash)" >> $CHANGELOG_FILE
-    elif [[ $commit_message == Changed* ]]; then
-        echo "## Changed" >> $CHANGELOG_FILE
-        echo "- $commit_message ($commit_hash)" >> $CHANGELOG_FILE
-    elif [[ $commit_message == Deprecated* ]]; then
-        echo "## Deprecated" >> $CHANGELOG_FILE
-        echo "- $commit_message ($commit_hash)" >> $CHANGELOG_FILE
-    elif [[ $commit_message == Removed* ]]; then
-        echo "## Removed" >> $CHANGELOG_FILE
-        echo "- $commit_message ($commit_hash)" >> $CHANGELOG_FILE
-    elif [[ $commit_message == Fixed* ]]; then
-        echo "## Fixed" >> $CHANGELOG_FILE
-        echo "- $commit_message ($commit_hash)" >> $CHANGELOG_FILE
-    elif [[ $commit_message == Security* ]]; then
-        echo "## Security" >> $CHANGELOG_FILE
-        echo "- $commit_message ($commit_hash)" >> $CHANGELOG_FILE
+# Obtenir les commits et vérifier qu'il y en a
+git log --pretty=format:"%h;%cd;%s" --date=short --reverse | while IFS=";" read commit_hash commit_date commit_message; do
+    # Ignorer les commits qui commencent par '#'
+    if [[ $commit_message == \#* ]]; then
+        continue
     fi
+
+    # Extraire le tag et le fichier/composant du message
+    tag=$(echo $commit_message | cut -d'(' -f1)
+    file_component=$(echo $commit_message | cut -d'(' -f2 | cut -d')' -f1)
+    description=$(echo $commit_message | cut -d')' -f2-)
+
+    # Classer les commits selon les tags et ajouter au changelog
+    case $tag in
+        Added)
+            echo "## Added" >> $CHANGELOG_FILE
+            ;;
+        Changed)
+            echo "## Changed" >> $CHANGELOG_FILE
+            ;;
+        Deprecated)
+            echo "## Deprecated" >> $CHANGELOG_FILE
+            ;;
+        Removed)
+            echo "## Removed" >> $CHANGELOG_FILE
+            ;;
+        Fixed)
+            echo "## Fixed" >> $CHANGELOG_FILE
+            ;;
+        Security)
+            echo "## Security" >> $CHANGELOG_FILE
+            ;;
+    esac
+
+    # Ajouter le commit avec ses informations
+    echo "| $commit_date | $commit_hash | $file_component | $description |" >> $CHANGELOG_FILE
 done
 
+# Ajouter un en-tête pour les colonnes
+sed -i '3i| Date       | Commit    | Fichier/Composant | Description |' $CHANGELOG_FILE
+sed -i '4i|------------|-----------|-------------------|-------------|' $CHANGELOG_FILE
+
+
+# Générer un fichier JSON
+JSON_FILE="changelog.json"
+echo "[" > $JSON_FILE
+
+git log --pretty=format:'{"commit": "%h", "date": "%cd", "message": "%s"},' --date=short --reverse | sed '$ s/,$//' >> $JSON_FILE
+
+echo "]" >> $JSON_FILE
+
+
+# Convertir le changelog en PDF
+pandoc CHANGELOG.md -o CHANGELOG.pdf
